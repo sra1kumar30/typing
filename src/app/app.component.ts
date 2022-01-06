9b98c0bb670ce6318883232314e78a573f9a980c
sra1kumar30 <sra1kumar30@gmail.com>
2022-01-06 22:52:32 +0100

    added typing logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomSentence = lorem.sentence();
+  typedText = '';
+  onUserTypeText(event: Event) {
+    const typedText = (<HTMLInputElement>event.target).value;
+    this.typedText = typedText;
+  }
+  isSuccessMessageShown() {
+    console.log(this.typedText);
+    return this.randomSentence === this.typedText;
+  }
+  compareForHighlighting(randomLetter: string, typedLetter: string) {
+    if (!typedLetter) {
+      return 'pending';
+    }
+    return randomLetter === typedLetter ? 'correct' : 'incorrect';
+  }
 }
